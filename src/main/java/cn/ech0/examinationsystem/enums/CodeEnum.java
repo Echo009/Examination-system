244e7ba7f34f1a7afa76948842a756ef7a31e140
Echo009 <ech0.extreme@foxmail.com>
2018-04-28 12:06:44 +0800

    utils

---
+++ src/main/java/cn/ech0/examinationsystem/enums/CodeEnum.java
@@ -0,0 +1,11 @@
+package cn.ech0.examinationsystem.enums;
+
+/**
+ * Author : Ech0
+ * Email  : ech0.extreme@foxmail.com
+ * Time   : 04/16/2018 10:15 PM
+ * @author Ech0
+ */
+public interface CodeEnum {
+    Integer getCode();
+}
